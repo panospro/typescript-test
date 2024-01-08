30ebaabb67a2d7ac468d3de7afab55d04bf951d4
protops <65860101+panospro@users.noreply.github.com>
2024-01-08 09:57:05 +0200

    Update index.ts

--- packages/typescript-build/src/index.ts
+++ packages/typescript-build/src/index.ts
@@ -95,4 +95,91 @@ const tsModule: Module<Options> = function (moduleOptions) {
   })
 }
 
+export interface Options {
+  ignoreNotFoundWarnings?: boolean
+  loaders?: {
+    ts?: Partial<TsLoaderOptions>
+    tsx?: Partial<TsLoaderOptions>
+  }
+  typeCheck?: TsCheckerOptions | boolean
+}
+
+declare module '@nuxt/types' {
+  interface NuxtOptions {
+    typescript: Options
+  }
+}
+
+const defaults: Options = {
+  ignoreNotFoundWarnings: false,
+  typeCheck: true
+}
+
+const tsModule: Module<Options> = function (moduleOptions) {
+  // Combine options
+  const options = defu(this.options.typescript, moduleOptions, defaults)
+
+  // Change color of CLI banner
+  this.options.cli.bannerColor = 'blue'
+
+  if (!this.options.extensions.includes('ts')) {
+    this.options.extensions.push('ts')
+  }
+
+  // Extend Builder to handle .ts/.tsx files as routes and watch them
+  this.options.build.additionalExtensions = ['ts', 'tsx']
+
+  if (options.ignoreNotFoundWarnings) {
+    this.options.build.warningIgnoreFilters!.push(warn =>
+      warn.name === 'ModuleDependencyWarning' && /export .* was not found in /.test(warn.message)
+    )
+  }
+
+  this.extendBuild((config, { isClient, isModern }) => {
+    config.resolve!.extensions!.push('.ts', '.tsx')
+
+    const jsxRuleLoaders = config.module!.rules.find(r => (r.test as RegExp).test('.jsx'))!.use as RuleSetUseItem[]
+    const babelLoader = jsxRuleLoaders[jsxRuleLoaders.length - 1]
+
+    config.module!.rules.push(...(['ts', 'tsx'] as const).map(ext =>
+      ({
+        test: new RegExp(`\\.${ext}$`, 'i'),
+        use: [
+          babelLoader,
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              appendTsxSuffixTo: ext === 'tsx' ? [/\.vue$/] : [],
+              ...(options.loaders && options.loaders[ext])
+            }
+          }
+        ]
+      })
+    ))
+
+    if (options.typeCheck && isClient && !isModern) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+      const logger = consola.withTag('nuxt:typescript')
+      /* istanbul ignore next */
+      const loggerInterface: TsCheckerLogger = {
+        log (message) { logger.log(message) },
+        info (message) { logger.info(message) },
+        error (message) { logger.error(message) }
+      }
+      config.plugins!.push(new ForkTsCheckerWebpackPlugin(defu(options.typeCheck, {
+        typescript: {
+          configFile: path.resolve(this.options.rootDir!, 'tsconfig.json'),
+          extensions: {
+            vue: true
+          }
+        },
+        logger: {
+          issues: loggerInterface
+        }
+      } as TsCheckerOptions)))
+    }
+  })
+}
 export default tsModule
